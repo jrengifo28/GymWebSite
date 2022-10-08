1a40302004fbbf878fab4ecc564b296a2a8a5d06
Jhonathan Rengifo Castillo <jrengifo28@hotmail.com>
2022-10-08 22:38:52 +0200

    se creó los metodos __str__ de los modelos y se agregaron los primeros valores de pruebas para probar los add a la BD.

--- PrimeraEntrega/AppGym/admin.py
+++ PrimeraEntrega/AppGym/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from AppGym.models import Cliente, Entrenador, Rutina
+
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Entrenador)
+admin.site.register(Rutina)
--- PrimeraEntrega/AppGym/models.py
+++ PrimeraEntrega/AppGym/models.py
@@ -1,30 +1,52 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 
 
 class Cliente(models.Model):
-    id = models.IntegerField()
-    nombre = models.CharField(max_length="40")
-    apellido = models.CharField(max_length="40")
+
+    class Meta:
+        verbose_name_plural = "Clientes"
+
+    codigo = models.IntegerField()
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
     phone = models.IntegerField()
-    direccion = models.CharField(max_length="100")
-    cumple = models.DateField()
+    direccion = models.CharField(max_length=60)
+    cumple = models.DateField(null=True)
+
+    def __str__(self):
+        return f"({self.codigo}) {self.nombre} {self.apellido}"
 
 
 class Entrenador(models.Model):
-    codigo_entrenador = models.IntegerField()
-    nombre = models.CharField(max_length="40")
-    apellido = models.CharField(max_length="40")
+
+    class Meta:
+        verbose_name_plural = "Entrenadores"
+
+    codigo = models.IntegerField()
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
     phone = models.IntegerField()
-    direccion = models.CharField(max_length="100")
-    cumple = models.DateField()
-    profesion = models.CharField(max_length="40")
-    tipo_entrenador = models.CharField(max_length="40")
+    direccion = models.CharField(max_length=40)
+    cumple = models.DateField(null=True)
+    profesion = models.CharField(max_length=40)
+    tipo_entrenador = models.CharField(max_length=40)
+
+    def __str__(self):
+        return f"({self.codigo}) {self.nombre} {self.apellido} - {self.tipo_entrenador}"
 
 
 class Rutina(models.Model):
-    id = models.IntegerField()
-    tipo_rutina = models.CharField(max_length="40")
-    ubicacion = models.CharField(max_length="40")
-    rutina = models.CharField(max_length="1000")
+
+    class Meta:
+        verbose_name_plural = "Rutinas"
+
+    codigo_rutina = models.IntegerField()
+    tipo_rutina = models.CharField(max_length=40)
+    ubicacion = models.CharField(max_length=40)
+    rutina = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"({self.codigo_rutina}) {self.tipo_rutina} - {self.ubicacion}"
