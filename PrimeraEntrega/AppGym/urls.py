9d8db3cafd55ff456b83d9e9f0691eb8f0dd03e0
Jimena Cambronero <jimenacambronero@gmail.com>
2022-10-09 00:42:15 -0300

    Formularios

--- PrimeraEntrega/AppGym/urls.py
+++ PrimeraEntrega/AppGym/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from AppGym.views import show_home, show_main
+from AppGym.views import show_home, show_main, procesar_formulario_cliente,procesar_formulario_entrenador,procesar_formulario_rutina
 
 urlpatterns = [
     path('main/', show_main),
-    path('inicio/', show_home)
+    path('inicio/', show_home),
+    path('formularioCliente/', procesar_formulario_cliente),
+    path('formularioEntrenador/', procesar_formulario_entrenador),
+    path('formularioRutina/', procesar_formulario_rutina),
+    
+    
+    
 ]
--- PrimeraEntrega/AppGym/views.py
+++ PrimeraEntrega/AppGym/views.py
@@ -3,10 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def show_home(request):
+    return render(request, "AppGym/inicio.html")
 
 def show_main(request):
     return render(request, "AppGym/main.html")
 
+def procesar_formulario_cliente(request):
+    if request.method != "POST":
+        return render (request, "AppGym/formularioCliente.html")
+    
+def procesar_formulario_entrenador(request):
+    if request.method != "POST":
+        return render (request, "AppGym/formularioEntrenador.html")
 
-def show_home(request):
-    return render(request, "AppGym/inicio.html")
+def procesar_formulario_rutina(request):
+    if request.method != "POST":
+        return render (request, "AppGym/formularioRutina.html")
