4501fce039e2ddb45e01724f019070f0cfeef05e
Jexson Montilla <jexson.montillau@gmail.com>
2022-10-10 01:22:52 -0300

    agrego algunos cambios

--- PrimeraEntrega/AppGym/urls.py
+++ PrimeraEntrega/AppGym/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from AppGym.views import (
     show_home,
     show_main,
+    buscar_cliente,
     procesar_formulario_cliente,
     procesar_formulario_entrenador,
     procesar_formulario_rutina,
@@ -12,6 +13,7 @@ from AppGym.views import (
 urlpatterns = [
     path("main/", show_main),
     path("inicio/", show_home),
+    path("buscarCliente/",buscar_cliente),
     path("formularioCliente/", procesar_formulario_cliente),
     path("formularioEntrenador/", procesar_formulario_entrenador),
     path("formularioRutina/", procesar_formulario_rutina),
